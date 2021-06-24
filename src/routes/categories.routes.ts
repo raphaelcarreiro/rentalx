6757530e6ad75435efcbbd97f7cd24fdf7d89b2f
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2021-06-24 17:30:39 -0300

    continuando implementação do swagger-ui

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -12,7 +12,7 @@ const upload = multer({
 const categoriesRoutes = Router();
 
 categoriesRoutes.post("/categories", (request, response) => createCategoryController.handle(request, response));
-categoriesRoutes.get("/categories", listCategoriesController.handle);
+categoriesRoutes.get("/categories", (request, response) => listCategoriesController.handle(request, response));
 categoriesRoutes.post("/import", upload.single("file"), (request, response) =>
   importCategoryController.handle(request, response),
 );
